94d0f9f612a2070509a60a7ce80a8d2f33a67a62
Guest User <Guest@Mubashirs-Mac-mini.local>
2014-08-28 15:41:48 +0530

    Initial Commit

---
+++ student/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  student
+//
+//  Created by Guest User on 8/28/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ student/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  student
+//
+//  Created by Guest User on 8/28/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
